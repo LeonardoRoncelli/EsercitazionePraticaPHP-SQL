de8c23987c538a2d56817b353985aca6d9bc1720
LeonardoRoncelli <160123628+LeonardoRoncelli@users.noreply.github.com>
2026-04-16 09:55:45 +0200

    Inserimento titolo

--- index.php
+++ index.php
@@ -4,4 +4,10 @@ if ($conn->connect_error) {
     die("Errore connessione");
 }
 ?>
+<?php
+if(isset($_POST['azione']) && $_POST['azione'] == 'libro'){
+    $conn->query("INSERT INTO Libri (titolo, anno_pubblicazione, isbn, id_autore)
+                  VALUES ('$_POST[titolo]', '$_POST[anno]', '$_POST[isbn]', '$_POST[id_autore]')");
+}
+?>
 <h1>Gestione Biblioteca</h1>
