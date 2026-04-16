1a0bb00426cf032541c847308677404e092d1cb7
LeonardoRoncelli <160123628+LeonardoRoncelli@users.noreply.github.com>
2026-04-16 09:57:39 +0200

    Form inserimento libro

--- index.php
+++ index.php
@@ -11,3 +11,21 @@ if(isset($_POST['azione']) && $_POST['azione'] == 'libro'){
 }
 ?>
 <h1>Gestione Biblioteca</h1>
+<h2>Inserisci Libro</h2>
+<form method="POST">
+<input type="hidden" name="azione" value="libro">
+Titolo: <input name="titolo" required>
+Anno: <input name="anno">
+ISBN: <input name="isbn">
+Autore:
+<select name="id_autore">
+<?php
+$res = $conn->query("SELECT * FROM Autori");
+while($a = $res->fetch_assoc()){
+    echo "<option value='{$a['id_autore']}'>{$a['nome']} {$a['cognome']}</option>";
+}
+?>
+</select>
+
+<button>Salva</button>
+</form>
